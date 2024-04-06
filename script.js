3f5a6f626851b5d8ab8ed78224465af8f78db68d
Bilal Zahid <126840907+billyboy381@users.noreply.github.com>
2024-04-06 23:38:22 +0300

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
- 
- function inputLength() {
- 	return input.value.length;
- }
 
 function createListElement() {
-	var li = document.createElement("li");
-	    li.appendChild(document.createTextNode(input.value));
-	     ul.appendChild(li);
-	     input.value = "";
+  var div = document.createElement("div");
+  var li = document.createElement("li");
+  var delButton = document.createElement("button");
+  div.classList.add("wrapper");
+  ul.appendChild(div);
+  div.append(li, delButton);
+  li.classList.add("taskClass");
+  li.appendChild(document.createTextNode(input.value));
+  input.value = "";
+  delButton.classList.add("delClass");
+  delButton.innerHTML='Delete';
 }
 
-function addListAfterClick(){
+function inputLength() {
+  return input.value.length;
+}
 
-	if (input.value.length() > 0) {
-        createListElement();
-	}
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (input.value.length > 0 && event.key === "Enter") {
-		 createListElement();
-	}
-	
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
 }
 
-button.addEventListener("click", addListAfterClick);
+function doneTask(task) {
+  if (task.target.tagName === "LI"){
+    task.target.classList.toggle("done");
+  }
+}
 
-input.addEventListener("keydown", addListAfterKeypress);
+function deleteListElement(element) {
+  if (element.target.className === "delClass"){
+    element.target.parentElement.remove();
+  }
+}
+
+function handleUlClick (element) {
+  doneTask(element);
+  deleteListElement(element);
+}
+
+ul.addEventListener("click", handleUlClick)
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
